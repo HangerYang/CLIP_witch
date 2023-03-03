1259c939c86233e4a81baf661055d20532171e0c
mxuan0 <mxuan0@gmail.com>
2023-03-02 19:03:04 -0800

    debug

--- poisoning-gradient-matching/forest/witchcoven/witch_matching_clip.py
+++ poisoning-gradient-matching/forest/witchcoven/witch_matching_clip.py
@@ -125,7 +125,10 @@ class WitchGradientMatchingClip(_Witch):
                     if target_grad[i] is None:
                         print("Target gradient is None at indice %d, 2 is logit_scale" %i)
                         continue
-                    passenger_loss += self.args.repel * (target_grad[i] * poison_grad[i]).sum()
+                    try:
+                        passenger_loss += self.args.repel * (target_grad[i] * poison_grad[i]).sum()
+                    except TypeError:
+                        pdb.set_trace()
                 elif self.args.loss == 'cosine1':
                     passenger_loss -= self.args.repel * torch.nn.functional.cosine_similarity(target_grad[i].flatten(), poison_grad[i].flatten(), dim=0)
                 elif self.args.loss == 'SE':
