be213411ca6fe943d2eb5d99dd0f5fd24c5eeb47
mxuan0 <mxuan0@gmail.com>
2023-03-02 17:38:43 -0800

    debug

--- poisoning-gradient-matching/forest/utils.py
+++ poisoning-gradient-matching/forest/utils.py
@@ -17,6 +17,7 @@ from .consts import NON_BLOCKING
 def system_startup(args=None, defs=None):
     """Decide and print GPU / CPU / hostname info."""
     device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device('cpu')
     setup = dict(device=device, dtype=torch.float, non_blocking=NON_BLOCKING)
     print('Currently evaluating -------------------------------:')
     print(datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p"))
